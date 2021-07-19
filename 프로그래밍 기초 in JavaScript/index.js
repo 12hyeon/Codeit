5e297406628cda68b3f32e103a3a6a48926faaf6
HyeonJeong <67951802+H-yeonJeong@users.noreply.github.com>
2021-07-19 12:47:47 +0900

    codeit's practice files (20.7.15 ~ 20.7.19)

---
+++ 프로그래밍 기초 in JavaScript/index.js
@@ -0,0 +1,14 @@
+//console.log('Hello Codeit!');
+console.log('Hello Codeit!');
+
+/*아래 코드는 
+15를 출력하는 코드입니다.*/
+console.timeLog(10 + 5); 
+
+// 변수 선언
+let espressoPrice = 3000;
+let lattePrice = 4300;
+let mochaPrice = 4800;
+console.log(espressoPrice * 2);
+console.timeLog(lattePrice + mochaPrice);
+console.log(espressoPrice);
